388775d1f20939bfeb03e5471921671e8bf7eafe
karanbro555 <90977560+karanbro555@users.noreply.github.com>
2023-09-12 21:23:57 +0530

    Update env.ts

--- env.ts
+++ env.ts
@@ -4,7 +4,7 @@ import { cleanEnv, str } from "envalid";
 await config({ export: true });
 
 export default cleanEnv(Deno.env.toObject(), {
-  BOT_TOKEN: str(),
-  OWNERS: str(),
+  BOT_TOKEN: str(6547420147:AAED2CrXzH-rlrM3i56Nvl9Tvkj-QP31ppc),
+  OWNERS: str(1229173534),
   MONGO_URL: str(mongodb+srv://renamedoge:renamedoge@cluster0.ag4ewhc.mongodb.net/?retryWrites=true&w=majority),
 });
